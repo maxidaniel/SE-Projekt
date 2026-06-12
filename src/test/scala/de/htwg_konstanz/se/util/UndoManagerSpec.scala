cdf31614351f31ce257e2383ff95cb8db86f1115
Shirosaka <shiro@shirosaka.dev>
2026-06-12 12:53:05 +0200

    fully cover undomanager

--- src/test/scala/de/htwg_konstanz/se/util/UndoManagerSpec.scala
+++ src/test/scala/de/htwg_konstanz/se/util/UndoManagerSpec.scala
@@ -9,4 +9,16 @@ import org.scalatest.wordspec.AnyWordSpec
 import java.util.UUID
 
 class UndoManagerSpec extends AnyWordSpec {
+  "An undo manager" should {
+    val undoManager = UndoManager()
+    "do no undo when undoStack is empty" in {
+      val postUndo = undoManager.undoStep()
+      postUndo should be(undoManager)
+    }
+
+    "do no redo when redoStack is empty" in {
+      val postRedo = undoManager.redoStep()
+      postRedo should be(undoManager)
+    }
+  }
 }
